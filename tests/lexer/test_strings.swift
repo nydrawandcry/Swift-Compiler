dab346161a36ee3ec58f8c5f302714dc9c89f86d
tyllcimun <annamihalna2005@gmail.com>
2026-09-18 02:36:31 +0300

    добавлены тесты для raw strings

--- tests/lexer/test_strings.swift
+++ tests/lexer/test_strings.swift
@@ -9,3 +9,22 @@ let s4 = "Quote: \"Swift\""
 let s5 = "Slash: \\"
 
 let s6 = "Quote & Slash: \\\"Swift\""
+
+
+
+
+let raw1 = #"Hello World"#
+
+let raw2 = #"Hello\nWorld"#
+
+let raw3 = #"Hello\tWorld"#
+
+let raw4 = #"He said "Hello""#
+
+let raw5 = #"C:\Users\Documents"#
+
+let raw6 = #"Hello\n\t\\\"World"#
+
+let raw7 = #""#
+
+let raw8 = #"Hello, World! @#$%^&*()_+"#
