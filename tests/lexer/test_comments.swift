a492d730e76a404fbd6fd7a67d51c88b4487df49
tyllcimun <annamihalna2005@gmail.com>
2026-09-18 00:11:22 +0300

    добавлены тесты для лексера

---
+++ tests/lexer/test_comments.swift
@@ -0,0 +1,15 @@
+// single line comment
+
+let x = 5
+
+/*
+   multiline
+   comment
+*/
+
+let y = 10
+
+/* outer
+    /* inner */
+   outer
+*/
---
+++ tests/lexer/test_keywords.swift
@@ -0,0 +1,16 @@
+let x = 10
+var y = 20
+
+if true {
+    print(x)
+} else {
+    return
+}
+
+while false {
+    break
+}
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
