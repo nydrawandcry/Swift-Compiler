ab88bdf6c34f7031a4a1f134f65e9d8fef6ca778
ная <nastya034rus@yandex.ru>
2026-09-18 10:51:53 +0300

    добавлен тестовый файл для проверки отлова ошибок в числах

---
+++ tests/lexer/test_numbers.swift
@@ -0,0 +1,12 @@
+let a = 123
+let b = 0b101
+let c = 0o755
+let d = 0xFF
+let e = 1.25e-2
+let f = 0xFp2
+
+let error1 = 0b
+let error2 = 0o8
+let error3 = 0xG
+let error4 = 0B101
+let error5 = 123abc
